d5afa02295b7ab1f64b73ce72ecf251ae47e91ee
aditigawali <aditigawali19@gmail.com>
2024-03-04 19:21:49 +0530

    Updated File

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import pickle
 import numpy as np
+import config
 def get_apple_quality(Size, Weight, Sweetness,Crunchiness,Juiciness,Ripeness,Acidity):
-    model_file_path = r"artifacts\KNN.pkl"
+    model_file_path = config.MODEL_FILE_PATH
 
     with open(model_file_path, 'rb') as f:
         model = pickle.load(f)
